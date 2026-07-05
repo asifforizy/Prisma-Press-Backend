0a78d0e93eeab9ae418fc07a74bcf6c3c5db2b8c
Asif Forizy <asifforizy@gmail.com>
2026-07-05 21:49:01 +0600

    setting up multi schea

--- src/server.ts
+++ src/server.ts
@@ -8,16 +8,18 @@ const PORT = config.port ;
 async function main() {
   try {
 
-    // await prisma.$connect();
+    await prisma.$connect();
     console.log("connect to database successfully");
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Error starting server:", error);
-    // await prisma.$disconnect();
+    await prisma.$disconnect();
     process.exit(1);
   }
 }
+
+
 
 main();
